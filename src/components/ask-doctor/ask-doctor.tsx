360fc29551c1a5811c3fa4860030dcd6e821137f
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2023-07-22 20:53:25 +0300

    feat(functions/shuffle-data): create randomizer of data

--- src/components/ask-doctor/ask-doctor.tsx
+++ src/components/ask-doctor/ask-doctor.tsx
@@ -1,12 +1,17 @@
 import CardQuestionDoctor from 'components/cards/question-doctor/question-doctor';
 import CardMoreContent from 'components/cards/more-content/more-content';
 import { Icon } from 'shared/icons';
+import shuffleData from 'utils/functions/shuffle-data';
 import data from './test-data/test-data';
 import styles from './ask-doctor.module.scss';
 
-const questions = data.map(({ id, heading, icon }) => (
-  <CardQuestionDoctor key={id} heading={heading} icon={icon} />
-));
+const maxNumCardsDesktop = 5;
+
+const questions = shuffleData(data, maxNumCardsDesktop).map(
+  ({ id, heading, icon }) => (
+    <CardQuestionDoctor key={id} heading={heading} icon={icon} />
+  )
+);
 
 export default function AskDoctor() {
   return (
--- src/components/ask-doctor/test-data/test-data.tsx
+++ src/components/ask-doctor/test-data/test-data.tsx
@@ -2,27 +2,27 @@ import { Icon } from 'shared/icons';
 
 const data = [
   {
-    id: 1,
+    id: '1',
     icon: <Icon icon="EyeIcon" size="80" color="white" />,
     heading: 'Офтальмология',
   },
   {
-    id: 2,
+    id: '2',
     icon: <Icon icon="VaccinationIcon" size="80" color="white" />,
     heading: 'Вакцинация',
   },
   {
-    id: 3,
+    id: '3',
     icon: <Icon icon="GastroenterologyIcon" size="80" color="white" />,
     heading: 'Гастроэнтерология',
   },
   {
-    id: 4,
+    id: '4',
     icon: <Icon icon="GeneticsIcon" size="80" color="white" />,
     heading: 'Генетика',
   },
   {
-    id: 5,
+    id: '5',
     icon: <Icon icon="GynecologyIcon" size="80" color="white" />,
     heading: 'Гинекология',
   },
