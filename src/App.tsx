6a76d4c495cde7da0070a15263f5d8fa8e543d1c
Aleksandr Dronov <dronovspb@gmail.com>
2023-07-22 18:53:09 +0300

    feat(app): add routing in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import Footer from 'components/footer/footer';
+import { Header } from 'components/header';
+import { MainPage } from 'pages/MainPage';
+import { Route, Routes } from 'react-router-dom';
+
 function App() {
-  return <p>Learn React</p>; // for tests
+  return (
+    <>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+        </Routes>
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
