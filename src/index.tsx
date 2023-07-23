c7794d1a6eb1d7e7c0550893f2728c2b535b53b2
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2023-07-23 18:28:31 +0300

    feat(package.json): init redux toolkit and create store

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from 'services/app/store';
 import './assets/styles/index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,9 +10,11 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
 reportWebVitals();
