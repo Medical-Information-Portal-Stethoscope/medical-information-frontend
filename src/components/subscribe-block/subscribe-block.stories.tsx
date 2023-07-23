337deda259f96806ca5f16735aefed5cc8d52cb3
Polina Che <perhaybe@gmail.com>
2023-07-23 04:40:48 +0300

    feat(subscribe-block): subscribe-block, new color: color-neutral-150 (global styles)

---
+++ src/components/subscribe-block/subscribe-block.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { BrowserRouter } from 'react-router-dom';
+
+import { SubscribeBlock } from '.';
+
+const meta = {
+  title: 'uikit/Components/SubscribeBlock',
+  component: SubscribeBlock,
+  tags: ['autodocs'],
+  decorators: [
+    (Story) => (
+      <BrowserRouter>
+        <Story />
+      </BrowserRouter>
+    ),
+  ],
+} as Meta<typeof SubscribeBlock>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
