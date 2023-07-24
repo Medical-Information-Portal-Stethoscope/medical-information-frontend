0a605d8917326249d2d449fdc29c8ce9ce2df727
Aleksandr Dronov <dronovspb@gmail.com>
2023-07-24 09:46:41 +0300

    refactor: add route podcasts

--- src/components/podcasts/index.tsx
+++ src/components/podcasts/index.tsx
@@ -1,36 +1,26 @@
 /* eslint-disable react/no-array-index-key */
-import { ReactNode } from 'react';
-
-import CardPodcast, {
-  ICardPodcastProps,
-} from 'components/cards/card-podcast/CardPodcast';
-
-import MorePodcast from 'components/MorePodcast/MorePodcast';
-
+import CardPodcast from 'components/cards/card-podcast/CardPodcast';
+import MorePodcast from 'components/cards/more-podcast/MorePodcast';
 import { podcastsData } from './data/podcastData';
-
 import styles from './styles.module.scss';
 
-export const Podcasts = (podcasts: ICardPodcastProps[]) => {
-  // tmp
-  podcasts = podcastsData;
+const enableNumPodcast = 5;
 
-  const enableNumPodcast = 5;
-  const visiblePodcasts = [...podcasts].splice(0, enableNumPodcast);
+export const Podcasts = () => {
+  // tmp
+  const podcasts = podcastsData
+    .splice(0, enableNumPodcast)
+    .map((podcast, idx) => (
+      <li key={idx}>
+        <CardPodcast title={podcast.title} link={podcast.link} />
+      </li>
+    ));
 
   return (
     <section className={styles.podcasts}>
       <h2 className={styles.podcasts__header}>Подкасты</h2>
       <div className={styles.podcasts__row}>
-        <ul className={styles.podcasts__links}>
-          {visiblePodcasts.map(
-            (podcast, idx): ReactNode => (
-              <li key={idx}>
-                <CardPodcast title={podcast.title} link={podcast.link} />
-              </li>
-            )
-          )}
-        </ul>
+        <ul className={styles.podcasts__links}>{podcasts}</ul>
         <MorePodcast title="Больше подкастов" link="#" />
       </div>
     </section>
