f275f705cc8cf136bca209dad6fae93d0953bba2
Polina Che <perhaybe@gmail.com>
2023-07-28 06:16:31 +0300

    feat(feat/article-preview-small): article-preview-small component for single article page

---
+++ src/components/articles-preview-small/index.tsx
@@ -0,0 +1,34 @@
+import { FC } from 'react';
+import { TArticle } from 'utils/types/article';
+import CardArticlePreview from 'components/cards/article-preview/article-preview';
+import { articlesExample } from './data/articles';
+
+import styles from './styles.module.scss';
+
+interface IArticlePreviewSmall {
+  data: TArticle[];
+}
+
+const maxNumCardsDesktop = 2;
+
+export const ArticlesPreviewSmall: FC<IArticlePreviewSmall> = ({
+  data = articlesExample,
+}) => {
+  const articles = data
+    .slice(0, maxNumCardsDesktop)
+    .map((item) => (
+      <CardArticlePreview
+        key={item.id}
+        data={item}
+        type="media"
+        extraClass={styles.articles__item}
+      />
+    ));
+
+  return (
+    <div className={styles.articles}>
+      <h3 className={styles.articles__header}>Еще по этой теме</h3>
+      <div className={styles.articles__containter}>{articles}</div>
+    </div>
+  );
+};
