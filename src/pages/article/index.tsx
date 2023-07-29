52fceac327bb1515c01a92e1d7196334eb502f0d
Polina Che <perhaybe@gmail.com>
2023-07-29 06:03:56 +0300

    feat(feat/news-page): news-page

---
+++ src/pages/article/index.tsx
@@ -0,0 +1,27 @@
+import { FC } from 'react';
+import { TArticle } from 'utils/types/article';
+import { ArticlesPreviewSmall } from 'components/articles-preview-small';
+import { Paper } from 'components/paper';
+import { useParams } from 'react-router-dom';
+import { newsExample } from './data/news';
+
+import styles from './styles.module.scss';
+
+interface IArticle {
+  data: TArticle[];
+}
+
+const tmpId = '72693381-ddb5-4585-b491-1621cf7f730a';
+
+export const Article: FC<IArticle> = ({ data = newsExample }) => {
+  const { id = tmpId } = useParams();
+  const selectedData = data.find((item) => item.id === id)!;
+  return (
+    <section className={styles.article}>
+      <div className={styles.article__container}>
+        <Paper data={selectedData} type="default" />
+        <ArticlesPreviewSmall data={data} />
+      </div>
+    </section>
+  );
+};
