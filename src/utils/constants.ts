bc109e578edcce22e9abab20ae368930335f2626
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2023-08-05 17:27:27 +0300

    feat(components/sidebar): create sticky sidebar with appearing button

---
+++ src/utils/constants.ts
@@ -0,0 +1,6 @@
+// HEADER
+export const footerHeightWithIndentsDesktopBig = 200;
+
+// FAVORITES CARDS
+export const cardFavoritesHeightDesktopBig = 438;
+export const cardFavoritesGapDesktopBig = 24;
