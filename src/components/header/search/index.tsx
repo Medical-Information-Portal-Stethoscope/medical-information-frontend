fcb102231afcccc92684459b23e14bc58d7e9e3a
Aleksandr Dronov <dronovspb@gmail.com>
2023-08-25 19:18:57 +0300

    fix: fix search input

--- src/components/header/search/index.tsx
+++ src/components/header/search/index.tsx
@@ -1,34 +1,33 @@
-import React, { FC, useState } from 'react';
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React, { FC, useCallback, useRef, useState } from 'react';
 import { SearchIcon } from 'shared/icons/search-icon';
 
 import styles from './styles.module.scss';
 
 export const Search: FC = () => {
   const [value, setValue] = useState<string>('');
-  const [isActive, setIsActive] = useState<boolean>(false);
+  const ref = useRef<HTMLInputElement>(null);
 
   function handleChange(event: React.ChangeEvent<HTMLInputElement>): void {
     setValue(event.target.value);
   }
 
-  function handleSearch(
-    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ): string {
+  function handleSearch(event: React.FormEvent<HTMLFormElement>): string {
     event.preventDefault();
     return value;
   }
 
-  function handleSetActive() {
-    setIsActive(true);
-  }
-  function handleSetInactive() {
-    setIsActive(false);
-  }
+  const onWrapperClick = useCallback(() => {
+    ref.current?.focus();
+  }, [ref]);
 
   return (
+    // eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions
     <form
-      className={`${styles.search} ${isActive ? styles.search_active : null}`}
+      className={styles.search}
       noValidate
+      onClick={onWrapperClick}
+      onSubmit={handleSearch}
     >
       <input
         className={styles.search__input}
@@ -37,17 +36,15 @@ export const Search: FC = () => {
         name="searchinput"
         placeholder="Поиск"
         required
+        ref={ref}
         value={value}
         onChange={handleChange}
-        onFocus={handleSetActive}
-        onBlur={handleSetInactive}
       />
 
       <button
         className={styles.search__submit}
-        type="button"
+        type="submit"
         name="search-info"
-        onClick={handleSearch}
       >
         <SearchIcon color="blue" size="24" />
       </button>
