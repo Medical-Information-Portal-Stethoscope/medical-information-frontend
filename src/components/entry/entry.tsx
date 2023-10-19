e3d6d81b51f2eb3aef7c73c854814ab0680a7487
Aleksandr Dronov <dronovspb@gmail.com>
2023-10-19 17:09:04 +0300

    feat(adaptive): adaptive for auth

--- src/components/entry/entry.tsx
+++ src/components/entry/entry.tsx
@@ -4,6 +4,9 @@ import classNames from 'classnames';
 import { Logo } from 'shared/logo';
 import Button from 'shared/buttons/button/button';
 import routes from 'utils/routes';
+import { useWindowDimensions } from 'hooks/useWindowDimensions';
+import { desktopMedium, tabletAlbumOrientation } from 'utils/constants';
+import { Header } from 'components/header';
 import styles from './entry.module.scss';
 
 interface IEntryProps {
@@ -33,12 +36,14 @@ const Entry: FC<IEntryProps> = ({
 }) => {
   const { pathname } = useLocation();
   const isSignUpForm = pathname.endsWith(routes.signup);
+  const isBelowMediumScreens = useWindowDimensions() < desktopMedium;
+  const isAboveTabletScreens = useWindowDimensions() > tabletAlbumOrientation;
 
   const content = (
     <>
       {hasCommentaryWithRequired && (
         <span className={styles.commentary}>
-          Поля со звёздочкой обязательны к заполнению
+          Поля со звёздочкой обязательны для заполнения
         </span>
       )}
       {children}
@@ -51,18 +56,22 @@ const Entry: FC<IEntryProps> = ({
         spinnerSize="small"
         spinnerColor="white"
         onClick={onClick}
+        size={isBelowMediumScreens ? 'small' : 'medium'}
       />
     </>
   );
 
   return (
     <main className={styles.main}>
-      <div className={styles.columnLeft}>
-        <Link className={styles.logo} to={routes.home}>
-          <Logo theme="dark" />
-          <span>медицинский информационный портал</span>
-        </Link>
-      </div>
+      {isAboveTabletScreens ? (
+        <div className={styles.columnLeft}>
+          <Link to={routes.home}>
+            <Logo theme="dark" isSignUp hasCaption />
+          </Link>
+        </div>
+      ) : (
+        <Header />
+      )}
       <div
         className={classNames(styles.columnRight, {
           [styles.columnRightSignUp]: isSignUpForm,
